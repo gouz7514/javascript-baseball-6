ff61f40fd2381355ba85d70bad58be9fcb50d8c9
gouz7514 <gouz7514@gmail.com>
2023-11-28 16:35:12 +0900

    Fix : GameController의 메소드를 private으로 선언

--- src/Controller/GameController.js
+++ src/Controller/GameController.js
@@ -12,26 +12,26 @@ class GameController {
   }
 
   async startGame() {
-    const computerNumber = this.generateComputerNumber();
-    const game = this.generateGame(computerNumber);
+    const computerNumber = this.#generateComputerNumber();
+    const game = this.#generateGame(computerNumber);
 
-    await this.playGame(game);
+    await this.#playGame(game);
   }
 
   // 1-2. 컴퓨터는 임의의 3자리 숫자를 생성한다.
-  generateComputerNumber() {
+  #generateComputerNumber() {
     return generateRandomNumber();
   }
 
-  generateGame(computerNumber) {
+  #generateGame(computerNumber) {
     const game = new Game(computerNumber);
     return game;
   }
 
   // 2. 숫자 야구 게임을 진행한다.
-  async playGame(game) {
+  async #playGame(game) {
     while (true) {
-      const userNumber = await this.getUserNumber();
+      const userNumber = await this.#getUserNumber();
       game.setUser(userNumber);
       const result = game.compareNumber();
       OutputView.printResult(result);
@@ -42,17 +42,17 @@ class GameController {
         break;
       }
     }
-    await this.getRestart();
+    await this.#getRestart();
   }
 
   // 2-1. 플레이어는 3자리의 숫자를 입력한다.
-  async getUserNumber() {
+  async #getUserNumber() {
     const userNumber = InputValidator.validateUserNumber(await InputView.getUserNumber());
     return userNumber;
   }
 
   // 2-4. 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료할 수 있다.
-  async getRestart() {
+  async #getRestart() {
     const answer = InputValidator.validateRestartNumber(await InputView.getRestart());
     if (answer === GAME.restart) {
       await this.startGame();
