c5f43446f1c06d218c0f7f275885f165dbe24529
gouz7514 <gouz7514@gmail.com>
2023-11-28 15:23:55 +0900

    Update : 상수 파일 내에서 상수 재사용

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,9 +3,15 @@ export const OUTPUT_MESSAGE = {
   endGame: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
 }
 
+export const GAME = {
+  restart: 1,
+  exit: 2,
+  end: 3,
+}
+
 export const INPUT_MESSAGE = {
   getUserNumber: '숫자를 입력해주세요 : ',
-  getRestart: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+  getRestart: `게임을 새로 시작하려면 ${GAME.restart}, 종료하려면 ${GAME.exit}를 입력하세요.\n`,
 }
 
 export const NUMBER = {
@@ -14,11 +20,6 @@ export const NUMBER = {
   max: 9,
 }
 
-export const GAME = {
-  restart: 1,
-  exit: 2,
-  end: 3,
-}
 
 export const ERROR_MESSAGE = {
   inValidLength: `[ERROR] 숫자는 ${NUMBER.length}자리만 입력 가능합니다.`,
